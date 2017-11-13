6a22c06f9ffaf933145aa081bb3e508c49e24d3a
Danil Alshanov <danila_a@ndv.ru>
2017-11-14 01:02:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import './App.css';
-import Header from './Header';
-import Content from './Content';
-=======
 import 'normalize.css';
 import styled from 'styled-components';
 import { Grid } from 'react-flexbox-grid';
@@ -23,17 +18,10 @@ const Wrapper = styled.div`
     display: flex;
   }
 `;
->>>>>>> 3a516ddcde20ca94183940de8479d7a1e96c352f
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <Header />
-        <Content />
-      </div>
-=======
       <Wrapper>
         <Grid>
           <Header />
@@ -45,7 +33,6 @@ class App extends Component {
           <Footer />
         </Grid>
       </Wrapper>
->>>>>>> 3a516ddcde20ca94183940de8479d7a1e96c352f
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-=======
->>>>>>> 3a516ddcde20ca94183940de8479d7a1e96c352f
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
